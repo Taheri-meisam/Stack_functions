c3f82f822e877494d21e6d64ad7e77b3d09d2d08
Meisam Taheri <meisam.taheri@inn.no>
2021-01-14 18:37:06 +0100

    Add project files.

---
+++ Stack_vec/Stack_vec.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <vector>
+#include<thread>
+
+typedef int func(void);
+std::vector<func*> functions;
+
+class FunMembers {
+public:
+    void addFuncToVec(func* f, std::vector<func*>& vect) {
+        vect.push_back(f);
+    }
+    FunMembers() {
+        func* f = (func*)0x000000;
+    }
+};
+int getA() {
+    std::cout << "GetA" << std::endl;
+    return 0;
+}
+
+int getB() {
+    std::cout << "GetB" << std::endl;
+    return 10;
+}
+int getC() {
+    std::cout << "GetC" << std::endl;
+    return 10;
+}
+int getD() {
+    std::cout << "GetD" << std::endl;
+    return 10;
+}
+int getE() {
+    std::cout << "GetE" << std::endl;
+    return 10;
+}
+
+int nothing() {
+    std::cout << " Nothing" << std::endl;
+    return 0;
+}
+
+int main()
+{
+
+    std::unique_ptr<FunMembers> fm;
+    fm->addFuncToVec(getA,functions);
+    fm->addFuncToVec(getB, functions);
+    fm->addFuncToVec(getC, functions);
+    fm->addFuncToVec(getD, functions);
+    fm->addFuncToVec(getE, functions);
+
+    for (auto i : functions) {
+        i();
+    }
+   
+}
+
